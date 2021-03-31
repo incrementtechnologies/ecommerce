35b22b6bc94eef18df7d1cff05d6100dbdf80c99
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-03-31 11:47:14 +0800

    add preview

--- product/TableView.vue
+++ product/TableView.vue
@@ -28,7 +28,7 @@
           </td>
           <td>
             <i class="fas fa-clone text-primary" v-if="item.type !== 'regular'" title="This is a bundled product"></i>
-            <label class="underline-on-hover" @click="redirect('/marketplace/product/' + (item.product_code ? item.product_code : item.code) )">{{item.title}} {{item.volume}}</label>
+            <label class="underline-on-hover" @click="redirect('/marketplace/product/' + (item.product_code ? item.product_code : item.code) + '/preview' )">{{item.title}} {{item.volume}}</label>
           </td>
           <td v-if="user.type === 'MANUFACTURER'">{{item.tags}}</td>
           <td v-if="user.type === 'MANUFACTURER'">
