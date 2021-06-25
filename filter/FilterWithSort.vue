ca9757c8dd07c222201ab9e0a9e46d80cb706c55
GarridoL <lalainecabelin@gmail.com>
2021-06-25 11:23:09 +0800

    updates

--- filter/FilterWithSort.vue
+++ filter/FilterWithSort.vue
@@ -295,6 +295,7 @@ export default {
       this.activeCategory = this.category[this.filterValue]
       this.sortValue = this.activeCategoryIndex
       this.level = this.category[this.filterValue].level
+      this.searchValue = ''
       this.changeSort()
     },
     changeSort(){
